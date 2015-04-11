e1233761eed2c8d36c712e2fc45f04c6ac52713f
saj1th <asajith@gmail.com>
2015-04-11 18:24:07 +0530

    package changes. added logging. basic try/catch

--- src/main/scala/org/bitbucket/saj1th/forecaster/Forecast.scala
+++ src/main/scala/org/bitbucket/saj1th/forecaster/Forecast.scala
@@ -1,7 +1,8 @@
-package org.ylabs.forecaster
+package org.bitbucket.saj1th.forecaster
 
 import java.util.Date
 import scala.collection.mutable.ArrayBuffer
+import scala.util.{Failure, Success, Try}
 
 import com.datastax.spark.connector.{SomeColumns, _}
 import org.apache.log4j.{Level, Logger}
@@ -11,7 +12,7 @@ import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.optimization.{L1Updater, SimpleUpdater, SquaredL2Updater}
 import org.apache.spark.mllib.regression.{LabeledPoint, LinearRegressionModel, LinearRegressionWithSGD}
 import org.apache.spark.rdd.RDD
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.{Logging, SparkConf, SparkContext}
 import org.joda.time.LocalDate
 import scopt.OptionParser
 
@@ -23,7 +24,7 @@ object RegularizationType extends Enumeration {
   type RegularizationType = Value
   val NONE, L1, L2 = Value
 }
-import org.ylabs.forecaster.RegularizationType._
+import RegularizationType._
 
 /**
  * command line params
@@ -36,7 +37,7 @@ case class Params(
                    regParam: Double = 0.01,
                    master: String = "local[2]",
                    predStart: String = "2015-01-01",
-                   predEnd: String = "2015-12-01",
+                   predEnd: String = "2015-12-31",
                    modelSavePath: String = "./",
                    cassandraHost: String = "127.0.0.1")
 
@@ -44,51 +45,50 @@ case class Params(
  * An  app for forecasting sale and volume
  * please use spark-submit to submit the app.
  */
-object Forecast {
+object Forecast extends App with Logging {
 
-  def main(args: Array[String]) {
-    val defaultParams = Params()
-
-    //Parse command line arguments
-    val parser = new OptionParser[Params]("Forecaster") {
-      opt[String]("master")
-        .text("master details")
-        .action((x, c) => c.copy(master = x))
-      opt[Int]("iterations")
-        .text("number of iterations")
-        .action((x, c) => c.copy(numIterations = x))
-      opt[Double]("stepsize")
-        .text(s"initial step size, default: ${defaultParams.stepSize}")
-        .action((x, c) => c.copy(stepSize = x))
-      opt[String]("cassandrahost")
-        .text("cassandra host")
-        .required()
-        .action((x, c) => c.copy(cassandraHost = x))
-      opt[String]("modelspath")
-        .text("path to save models to")
-        .required()
-        .action((x, c) => c.copy(modelSavePath = x))
-      arg[String]("<input>")
-        .required()
-        .text("path to train data file")
-        .action((x, c) => c.copy(data = x))
-      note(
-        """
-          |e.g.
-          |
-          | bin/spark-submit --class org.ylabs.forecaster.Predict \
-          |  /path/to/forecaster.1.0.jar \
-          |  --cassandraHost host.name.or.ip  \
-          |  /path/to/csv/data.txt
-        """.stripMargin)
-    }
-
-    parser.parse(args, defaultParams).map { params =>
-      run(params)
-    } getOrElse {
-      sys.exit(1)
-    }
+  val defaultParams = Params()
+  //Parse command line arguments
+  val parser = new OptionParser[Params]("Forecaster") {
+    opt[String]("master")
+      .text("master details")
+      .action((x, c) => c.copy(master = x))
+    opt[Int]("iterations")
+      .text("number of iterations")
+      .action((x, c) => c.copy(numIterations = x))
+    opt[Double]("stepsize")
+      .text(s"initial step size, default: ${defaultParams.stepSize}")
+      .action((x, c) => c.copy(stepSize = x))
+    opt[String]("cassandrahost")
+      .text("cassandra host")
+      .required()
+      .action((x, c) => c.copy(cassandraHost = x))
+    opt[String]("modelspath")
+      .text("path to save models to")
+      .required()
+      .action((x, c) => c.copy(modelSavePath = x))
+    arg[String]("<input>")
+      .required()
+      .text("path to train data file")
+      .action((x, c) => c.copy(data = x))
+    note(
+      """
+        |e.g.
+        |
+        | bin/spark-submit --class org.ylabs.forecaster.Predict \
+        |  /path/to/forecaster.1.0.jar \
+        |  --cassandrahost host.name.or.ip  \
+        |  --modelspath  hdfs://path/to/save/models \
+        |  hdfs://path/to/csv/data.txt
+      """.stripMargin)
   }
+
+  parser.parse(args, defaultParams).map { params =>
+    run(params)
+  } getOrElse {
+    sys.exit(1)
+  }
+
 
   /**
    * Runs the algorithm
@@ -98,7 +98,11 @@ object Forecast {
       .setAppName(s"Forecaster with $params")
       .setMaster(params.master)
       .set("spark.executor.memory", "1g")
+      .set("spark.hadoop.validateOutputSpecs", "false") //TODO: handle model overwrites elegantly
       .set("spark.cassandra.connection.host", params.cassandraHost)
+      //check spark.cassandra.* for additional parameters if needed
+
+    logInfo("running forecaster with" + params)
 
     val sc = new SparkContext(conf)
 
@@ -138,63 +142,92 @@ object Forecast {
     var volumeModels: Map[String, LinearRegressionModel] = Map()
     var saleModels: Map[String, LinearRegressionModel] = Map()
     var scalerModels: Map[String, StandardScalerModel] = Map()
+
+    logInfo("training models")
     for (sku <- ProductData.skus) {
       trainVolumeModel(sku, dailyVolume, algorithm) match {
-        case (volModel, scalerModel) =>
+        case (Success((volModel, scalerModel))) =>
           volumeModels += (sku -> volModel)
           scalerModels += (sku -> scalerModel)
+
           //scalerModel needs to be calculated only once
-          saleModels += (sku -> trainSaleModel(sku, dailySale, algorithm, scalerModel))
+          trainSaleModel(sku, dailySale, algorithm, scalerModel) match {
+            case (Success((saleModel))) =>
+              saleModels += (sku -> saleModel)
+            case Failure(ex) =>
+              logError("Failed to train sale model for sku:" + sku)
+              logError(s"${ex.getMessage}")
+          }
+        case Failure(ex) =>
+          logError("Failed to train volume model for sku:" + sku)
+          logError(s"${ex.getMessage}")
       }
     }
 
     // Do prediction
+    logInfo("do prediction")
     val predictions = predictFuture(volumeModels, saleModels, scalerModels, params.predStart, params.predEnd)
 
     // Save to Cassandra
+    logInfo("saving predictions to cassandra")
     val predictionsRdd = sc.parallelize(predictions)
     predictionsRdd.saveToCassandra("forecaster", "predictions", SomeColumns("sku", "date", "sale", "volume"))
 
     //Save models to HDFS
+    logInfo("saving models to HDFS")
     persistModels(volumeModels, saleModels, scalerModels, sc, params.modelSavePath)
 
+    logInfo("done!")
     sc.stop()
   }
 
   // Trains the Volume Model
   def trainVolumeModel(sku: String,
                        volumes: RDD[Volume],
-                       algorithm: LinearRegressionWithSGD): (LinearRegressionModel, StandardScalerModel) = {
-    val labelData = volumes
-      .filter(row => row.sku == sku)
-      .map { row => LabeledPoint(row.volume, Vectors.dense(row.year, row.month, row.day)) }
-    //Feature scaling to standardize the range of independent variables
-    val scaler = new StandardScaler(withMean = true, withStd = true).fit(labelData.map(x => x.features))
-    val scaledData = labelData
-      .map { data => LabeledPoint(data.label, scaler.transform(Vectors.dense(data.features.toArray))) }
-      .cache()
+                       algorithm: LinearRegressionWithSGD): Try[(LinearRegressionModel, StandardScalerModel)] = {
+    try {
+      //Create labeled data
+      val labelData = volumes
+        .filter(row => row.sku == sku)
+        .map { row => LabeledPoint(row.volume, Vectors.dense(row.year, row.month, row.day)) }
 
-    //Train the algo
-    val model = algorithm.run(scaledData)
-    (model, scaler)
+      //Feature scaling to standardize the range of independent variables
+      val scaler = new StandardScaler(withMean = true, withStd = true).fit(labelData.map(x => x.features))
+      val scaledData = labelData
+        .map { data => LabeledPoint(data.label, scaler.transform(Vectors.dense(data.features.toArray))) }
+        .cache()
+
+      //Train the algo
+      val model = algorithm.run(scaledData)
+      Success(model, scaler)
+    } catch {
+      case e: Exception => Failure(e)
+    }
   }
 
   // Trains the Sale Model
   def trainSaleModel(sku: String,
                      sales: RDD[Sale],
                      algorithm: LinearRegressionWithSGD,
-                     scaler: StandardScalerModel): LinearRegressionModel = {
-    val labelData = sales
-      .filter(row => row.sku == sku)
-      .map { row => LabeledPoint(row.sale, Vectors.dense(row.year, row.month, row.day)) }
-    //Feature scaling
-    val scaledData = labelData
-      .map { data => LabeledPoint(data.label, scaler.transform(Vectors.dense(data.features.toArray))) }
-      .cache()
+                     scaler: StandardScalerModel): Try[LinearRegressionModel] = {
 
-    //Train the algo
-    val model = algorithm.run(scaledData)
-    model
+    try {
+      //Create labeled data
+      val labelData = sales
+        .filter(row => row.sku == sku)
+        .map { row => LabeledPoint(row.sale, Vectors.dense(row.year, row.month, row.day)) }
+      //Feature scaling
+      val scaledData = labelData
+        .map { data => LabeledPoint(data.label, scaler.transform(Vectors.dense(data.features.toArray))) }
+        .cache()
+
+      //Train the algo
+      val model = algorithm.run(scaledData)
+      Success(model)
+    } catch {
+      case e: Exception => Failure(e)
+    }
+
   }
 
   // Perform prediction
@@ -203,6 +236,7 @@ object Forecast {
                     scalerModels: Map[String, StandardScalerModel],
                     predStart: String,
                     predEnd: String): ArrayBuffer[Prediction] = {
+    //Get the day iterator
     val itr = dayStream(new LocalDate(predStart), new LocalDate(predEnd))
     var predictions = new ArrayBuffer[Prediction]()
     for (sku <- ProductData.skus) {
@@ -222,23 +256,33 @@ object Forecast {
     predictions
   }
 
+  // Iterate over date ranges
+  def dayStream(start: LocalDate, end: LocalDate) = Stream.iterate(start)(_ plusDays 1) takeWhile (_ isBefore end)
+
   //Persist Model Files
   def persistModels(volumeModels: Map[String, LinearRegressionModel],
                     saleModels: Map[String, LinearRegressionModel],
                     scalerModels: Map[String, StandardScalerModel],
                     sc: SparkContext,
                     path: String) = {
-
-    for ((sku, model) <- volumeModels) {
-      sc.parallelize(Seq(model), 1).saveAsObjectFile(path + "/volume." + sku + ".model")
-    }
-
-    for ((sku, model) <- saleModels) {
-      sc.parallelize(Seq(model), 1).saveAsObjectFile(path + "/sale." + sku + ".model")
-    }
-
-    for ((sku, model) <- scalerModels) {
-      sc.parallelize(Seq(model), 1).saveAsObjectFile(path + "/scaler." + sku + ".model")
+    try {
+      //Save volume models
+      for ((sku, model) <- volumeModels) {
+        sc.parallelize(Seq(model), 1).saveAsObjectFile(path + "/volume." + sku + ".model")
+      }
+      //Save sale models
+      for ((sku, model) <- saleModels) {
+        sc.parallelize(Seq(model), 1).saveAsObjectFile(path + "/sale." + sku + ".model")
+      }
+      //Save scaler models
+      for ((sku, model) <- scalerModels) {
+        sc.parallelize(Seq(model), 1).saveAsObjectFile(path + "/scaler." + sku + ".model")
+      }
+    } catch {
+      case e: Exception => {
+        logError("Failed to save models to:" + path)
+        logError(s"${e.getMessage}")
+      }
     }
   }
 
@@ -274,13 +318,8 @@ object Forecast {
     println("Model: " + model.weights)
     println("Mean Square Error: " + MSE)
   }
-
-  // Iterate over date ranges
-  def dayStream(start: LocalDate, end: LocalDate) = Stream.iterate(start)(_ plusDays 1) takeWhile (_ isBefore end)
 }
 
-case class Sale(sku: String, year: Int, month: Int, day: Int, sale: Float)
-
 case class Prediction(sku: String, date: Date, sale: Long, volume: Long)
-
+case class Sale(sku: String, year: Int, month: Int, day: Int, sale: Float)
 case class Volume(sku: String, year: Int, month: Int, day: Int, volume: Int)
